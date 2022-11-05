98659fc470059db33ebd2a3a17fb0bbb4ad6462b
Bryan Souza <brybryflys@gmail.com>
2022-11-04 21:27:18 -0700

    refactored hello *name* again;

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Bryan")
-	want := "Hello, Bryan!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Bryan")
+		want := "Hello, Bryan!"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello, wonderful world!' when an empty string is entered", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, wonderful world!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
