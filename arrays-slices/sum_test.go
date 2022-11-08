37f054ecc8e02b4120f2b1eee4144ed4c8e5a6bc
Bryan Souza <brybryflys@gmail.com>
2022-11-08 09:24:57 -0800

    arrays and slices section passing tests and refactored;

--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -3,15 +3,26 @@ package arraysslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("set of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	assertCorrectMessage(t, got, want, numbers)
+		assertCorrectMessage(t, got, want, numbers)
+	})
+
+	t.Run("set of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		assertCorrectMessage(t, got, want, numbers)
+	})
 }
 
-func assertCorrectMessage(t testing.TB, got, want int, numbers [5]int) {
+func assertCorrectMessage(t testing.TB, got, want int, numbers []int) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %d want %d, given %v", got, want, numbers)
