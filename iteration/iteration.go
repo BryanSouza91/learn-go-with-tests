15eae18a5cec61bde974f39cd53bf75eeddc5906
Bryan Souza <brybryflys@gmail.com>
2022-11-07 23:00:49 -0800

    refactored iteration; added benchmarking to iteration;

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCount = 3
+
 func Repeat(char string) string {
 	var repeated string
-	for i := 0; i < 3; i++ {
-		repeated = repeated + char
+	for i := 0; i < repeatCount; i++ {
+		repeated += char
 	}
 	return repeated
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -8,10 +8,17 @@ func TestRepeat(t *testing.T) {
 
 	assertCorrectMessage(t, repeated, expected)
 }
+
 func assertCorrectMessage(t testing.TB, repeated, expected string) {
 	t.Helper()
 	if repeated != expected {
 		t.Errorf("expected %s but got %s", expected, repeated)
 	}
 
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
 }
