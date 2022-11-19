82d6f1f49ce18a3349e75ad1159e0294571cb3d3
Bryan Souza <brybryflys@gmail.com>
2022-11-19 01:32:40 -0800

    fixed the error causing the test to fail by refactoring and realizing I had forgot the ! (not) operator; this is why we test;

--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -42,22 +42,27 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	t.Run("sums the tails of some slices", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2}, []int{1, 2, 3, 4}, []int{1, 3, 5})
-		want := []int{2, 4, 5}
 
-		if reflect.DeepEqual(got, want) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("sums the tails of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{1, 2, 3, 4}, []int{1, 3, 5})
+		want := []int{2, 9, 8}
+
+		checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slice", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 
-		if reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
+
 	})
 
 }
