e1fb171373c97faf8dd0adaf34d599f092a245b7
Bryan Souza <brybryflys@gmail.com>
2022-11-20 01:03:32 -0800

    structs-methods-interfaces section going well;

--- structs-methods-interfaces/perimeter.go
+++ structs-methods-interfaces/perimeter.go
@@ -1,14 +1,24 @@
 package structsmethodsinterfaces
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- structs-methods-interfaces/perimeter_test.go
+++ structs-methods-interfaces/perimeter_test.go
@@ -2,6 +2,10 @@ package structsmethodsinterfaces
 
 import "testing"
 
+type Shape interface {
+	Area() float64
+}
+
 func TestPerimeter(t *testing.T) {
 	rect := Rectangle{10.0, 10.0}
 	got := Perimeter(rect)
@@ -13,11 +17,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{10.0, 10.0}
-	got := Area(rect)
-	want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10, 10}, 100.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
 	}
 }
