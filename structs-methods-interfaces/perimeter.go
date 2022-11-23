5755bbc664185f8e4f7ba094f051ba159e9b2247
Bryan Souza <brybryflys@gmail.com>
2022-11-22 18:22:33 -0800

    finished the structs, methods and interfaces section;

--- structs-methods-interfaces/perimeter.go
+++ structs-methods-interfaces/perimeter.go
@@ -11,6 +11,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
 }
@@ -21,4 +26,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
--- structs-methods-interfaces/perimeter_test.go
+++ structs-methods-interfaces/perimeter_test.go
@@ -19,17 +19,19 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{10, 10}, 100.0},
-		{Circle{10}, 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{Width: 10, Height: 10}, hasArea: 100.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
 		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
 	}
 }
