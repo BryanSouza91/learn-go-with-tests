31686fe95097dff289e39845ddfc95865543de1b
Bryan Souza <brybryflys@gmail.com>
2024-05-07 16:47:40 -0700

    finished concurrency section;

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -1,6 +1,7 @@
 package concurrency
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -18,5 +19,29 @@ func BenchmarkCheckWebsites(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
+
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds"
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v, got %v", want, got)
 	}
 }
