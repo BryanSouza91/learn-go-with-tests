d0e6f49b3704c62b3bd20036b2f1f9cb5a19208d
Bryan Souza <brybryflys@gmail.com>
2024-05-08 13:31:03 -0700

    started context section;

---
+++ context/context.go
@@ -0,0 +1,17 @@
+package context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
---
+++ context/go.mod
@@ -0,0 +1,3 @@
+module learn-go-with-tests/context
+
+go 1.22.2
