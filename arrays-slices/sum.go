00aed574d0b9ecf6f8a58528e3ccf2dcbbc2efcf
Bryan Souza <brybryflys@gmail.com>
2024-05-16 14:42:24 -0700

    started on the generic arrays and slices section; SumAll fails even though outputs appear the same;

--- arrays-slices/sum.go
+++ arrays-slices/sum.go
@@ -1,33 +1,55 @@
 package arraysslices
 
-func Sum(numbers []int) int {
-	sum := 0
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
 }
 
-func SumAll(arraysToSum ...[]int) []int {
-	lenOfNumbers := len(arraysToSum)
-	sums := make([]int, lenOfNumbers)
-
-	for _, arr := range arraysToSum {
-		sums = append(sums, Sum(arr))
-	}
-	return sums
-}
-
-func SumAllTails(arraysToSum ...[]int) []int {
-	var tailsums []int
-
-	for _, arr := range arraysToSum {
-		if len(arr) == 0 {
-			tailsums = append(tailsums, 0)
-		} else {
-			tail := arr[1:]
-			tailsums = append(tailsums, Sum(tail))
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
+		}
+		if t.To == name {
+			balance += t.Sum
 		}
 	}
-	return tailsums
+	return balance
+}
+
+// Sum calculates the total from a slice of numbers.
+func Sum(numbers []int) int {
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
+}
+
+// func SumAll(numbers ...[]int) []int {
+// 	sumAll := func(acc, x []int) []int {
+// 		return append(acc, Sum(x))
+// 	}
+// 	return Reduce(numbers, sumAll, []int{})
+// }
+
+// SumAllTails calculates the sums of all but the first number given a collection of slices.
+func SumAllTails(numbers ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
+		} else {
+			tail := x[1:]
+			return append(acc, Sum(tail))
+		}
+	}
+
+	return Reduce(numbers, sumTail, []int{})
+}
+
+func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = accumulator(result, x)
+	}
+	return result
 }
